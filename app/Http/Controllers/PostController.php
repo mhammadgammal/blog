4c9bfdd5d0e2302685018f869e9cc4378a4dd15b
mhammadgammal <mhammadgamal17@gmail.com>
2025-11-28 01:21:25 +0200

    execute find a post by id and map data into view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,14 +17,9 @@ class PostController extends Controller
     // show a single post
     public function show($id)
     {
-        $allPosts = [
-            ['id' => 1, 'title' => 'PHP', 'posted_by' => 'John Doe', 'created_at' => '2024-01-01'],
-            ['id' => 2, 'title' => 'JavaScript', 'posted_by' => 'Ahmed', 'created_at' => '2024-01-02'],
-            ['id' => 3, 'title' => 'HTML', 'posted_by' => 'Mohamed', 'created_at' => '2024-01-05'],
-            ['id' => 4, 'title' => 'CSS', 'posted_by' => 'Alice', 'created_at' => '2024-02-10'],
-        ];
+        $post = Post::find($id);
         // Logic to retrieve a single post by its ID
-        return view('posts.show', ['post' => $allPosts[$id - 1]]);
+        return view('posts.show', ['post' => $post]);
     }
 
     public function create()
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('title')
-    {{ $post['title'] }}
+    {{ $post->title }}
 @endsection
 @section('content')
     <div class="card" style="margin:10px;">
@@ -11,19 +11,19 @@
 
         </div>
         <div class="card-body">
-            <h5 class="card-title">Title: {{ $post['title'] }}</h5>
+            <h5 class="card-title">Title: {{ $post->title }}</h5>
             <p class="card-text">Description: This is Description</p>
         </div>
     </div>
     <div class="card" style="margin:10px;">
         <div class="card">
             <div class="card-header">
-                Post Createor Info
+                Post Info
             </div>
 
         </div>
         <div class="card-body">
-            <h5 class="card-title">Name: {{ $post['posted_by'] }}</h5>
-            <p class="card-text">Created at: {{ $post['created_at'] }}</p>
+            <h5 class="card-title">Name: {{ $post->description }}</h5>
+            <p class="card-text">Created at: {{ $post->created_at }}</p>
         </div>
     @endsection
